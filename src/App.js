80f713a7f0bdc5218a8779135eecbda5d6bc142c
Ishaq Kassam <ishaq.kassam@gmail.com>
2021-11-17 13:58:55 +0300

    setup some packages - styled-components and react-router-dom - do npm install to access them. - setup routing for the project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
+//do imports of packages here
+//styled-components allow us to create custom tags and style them within the js file
+import styled from 'styled-components' // do npm i styled-components to get this package
+// react-router-dom used for routing
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';//do npm install react-router-dom to get this package 
+
+
+
+// do elements(components) imports here
+import Home from './components/Home';
+
+
 
 function App() {
   return (
-    <div>
-    Hello
-    </div>
+    <Container>
+      <Router>
+        <Routes>
+          {/* in the new react router dom, you need to use the element={<component_name/>} */}
+          <Route exact path="/" element={<Home/>} />
+        </Routes>
+      </Router>
+    </Container>       
   );
 }
 
 export default App;
+
+const Container = styled.div`
+
+`
