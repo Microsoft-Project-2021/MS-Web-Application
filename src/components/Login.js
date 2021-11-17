eba2fc40fa070f71d76abaa9de8d8e3480c1d1b8
Ishaq Kassam <ishaq.kassam@gmail.com>
2021-11-17 16:45:00 +0300

    used useNavigate from react-router-dom to redirect to the home page after clicking the login button - working

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,21 +1,29 @@
 import styled from 'styled-components'
+import {Link, useNavigate} from 'react-router-dom'
 
 function Login() {
+
+    const navigate = useNavigate()
 
 const loginFunction = (e) => {
     e.preventDefault();
 
     console.log("Login button pressed")
+
+    navigate('/')
 }
 
     return (
         <Container>
             <LoginContainer>
+                <h3>Log In</h3>
                 <form onSubmit={loginFunction}>
                     <UserID>
+                        <label>User ID:</label>
                         <input/>
                     </UserID>
                     <Password>
+                        <label>Password:</label>
                         <input/>
                     </Password>
                     <LoginButton>
@@ -38,22 +46,52 @@ const Container = styled.div`
 const LoginContainer = styled.div`
     border: 1px solid black;
     margin: 50px 10px 0 70%;
-    display: flex;
+    /* display: flex; */
     justify-content: center;
+    /* align-items: center; */
+    
+    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;   
+
+    h3{
+        text-align: center;
+        margin-top: 20px;
+        /* border: 1px solid black; */
+
+    }
 
     form{
-        border: 1px solid black;
-        margin: 30px;
+        /* border: 1px solid black; */
+        padding: 0 25px;
+        margin: 20px;
         display: flex;
+        flex-wrap: wrap;
         flex-direction: column;
+        justify-content: center;
+
+        input{
+            width: 70%;
+        }
+
+
     }
 `
 const UserID = styled.div`
-
+    display: flex;
+    justify-content: space-between;
 `
 const Password = styled.div`
-
+    display: flex;
+    justify-content: space-between;
 `
 const LoginButton = styled.div`
+    /* border: 1px solid black; */
+    margin-top: 15px;
+    display: flex;
+    justify-content: right;
+    width: 100%;
 
+    button{
+        width: 50%;
+        margin-right: 11%;
+    }
 `
