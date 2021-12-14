d33b81cd039c96f0efdb4d29b0b1e76f0d451e6e
Tori-Stark <61185176+Tori-Stark@users.noreply.github.com>
2021-12-14 10:34:46 +0300

    Chat functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 //do imports of packages here
-import React from 'react';
+import React, {Component} from 'react';
 //styled-components allow us to create custom tags and style them within the js file
 import styled from 'styled-components' // do npm i styled-components to get this package
 // react-router-dom used for routing
@@ -15,8 +15,12 @@ import Register from './components/Register';
 import {AuthProvider} from './AuthContext'
 import Dashboard from './components/Dashboard';
 import PrivateRoute from './components/PrivateRoute';
+import Chat from './components/Chat';
+import {Switch} from "@material-ui/core";
 
 function App() {
+
+
  
   return (
     
@@ -32,6 +36,7 @@ function App() {
           <Route path="/register" element={<Register/>} />
           <Route element={<PrivateRoute/>}>
           <Route path="/dashboard" element={<Dashboard/>} />
+              <Route path="/chat" element={<Chat/>} />
           <Route path="/video" element={<Video/>} />
           </Route>
         </Routes>
@@ -41,6 +46,7 @@ function App() {
         
   );
 }
+
 
 export default App;
 
--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,6 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, } from "firebase/auth"; 
+import { getAuth, } from "firebase/auth";
+import {getFirestore, } from "firebase/firestore";
+import {getDatabase, } from "firebase/database";
+import firebase from "firebase/compat";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,5 +19,11 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const app2 = firebase.initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+export const fire = getFirestore(app);
+export const db = getDatabase(app);
+export const auth2 = firebase.auth;
+export const db2 = app2.database;
+export const fire2 = app2.firestore;
